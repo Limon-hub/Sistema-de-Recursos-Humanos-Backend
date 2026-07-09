325897bbe25a2d8f847474370d92484b9eb83fc9
Jesus David Limon Hernandez <jesus_limonhdz@hotmail.com>
2026-07-08 19:27:31 -0600

    Creando controlador Rest

---
+++ src/main/java/prh/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,21 @@
+package prh.rh.controlador;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import prh.rh.servicio.EmpleadoServicio;
+import prh.rh.servicio.IEmpleadoServicio;
+
+@RestController
+@RequestMapping("rh-app")
+@CrossOrigin(value = "http://localhost:3000")
+public class EmpleadoControlador {
+    private static final Logger logger =
+            LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @Autowired
+    private IEmpleadoServicio empleadoServicio;
+}
